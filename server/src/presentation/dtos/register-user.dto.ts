cb74a625c9651a2114b87d02380e6ba591f85b39
nguyentin082 <nguyentin082@gmail.com>
2025-07-14 13:55:10 +0700

    fix: backend swagger api example

--- server/src/presentation/dtos/register-user.dto.ts
+++ server/src/presentation/dtos/register-user.dto.ts
@@ -1,15 +1,34 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty, MinLength } from 'class-validator';
 
 export class RegisterUserDto {
-  @IsEmail()
-  email: string;
+    @ApiProperty({
+        description: 'Email of the user',
+        example: 'test@gmail.com',
+    })
+    @IsNotEmpty()
+    @IsEmail()
+    email: string;
 
-  @MinLength(6)
-  password: string;
+    @ApiProperty({
+        description: 'Password of the user',
+        example: '123456',
+    })
+    @MinLength(6)
+    @IsNotEmpty()
+    password: string;
 
-  @IsNotEmpty()
-  firstName: string;
+    @ApiProperty({
+        description: 'First name of the user',
+        example: 'Test',
+    })
+    @IsNotEmpty()
+    firstName: string;
 
-  @IsNotEmpty()
-  lastName: string;
+    @ApiProperty({
+        description: 'Last name of the user',
+        example: 'User',
+    })
+    @IsNotEmpty()
+    lastName: string;
 }
