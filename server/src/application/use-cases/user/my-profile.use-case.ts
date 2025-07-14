a3e47115d5bb3d06b03139c999022fccdc1d2a5c
nguyentin082 <nguyentin082@gmail.com>
2025-07-14 14:19:20 +0700

    fix: my-profile use-case

--- server/src/application/use-cases/user/my-profile.use-case.ts
+++ server/src/application/use-cases/user/my-profile.use-case.ts
@@ -3,13 +3,13 @@ import { User } from 'src/core/entities/user.entity';
 import { UserRepository } from 'src/core/repositories/user.repository';
 
 export class MyProfileUseCase {
-  constructor(
-    @Inject('UserRepository')
-    private readonly userRepository: UserRepository,
-  ) {}
-  async execute(id: string): Promise<User> {
-    const user = await this.userRepository.findById(id);
-    if (!user) throw new Error('User not found');
-    return user;
-  }
+    constructor(
+        @Inject('UserRepository')
+        private readonly userRepository: UserRepository
+    ) {}
+    async execute(id: string): Promise<User> {
+        const user = await this.userRepository.findById(id);
+        if (!user) throw new Error('User not found');
+        return user;
+    }
 }
