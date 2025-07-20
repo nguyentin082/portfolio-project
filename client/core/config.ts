14c6bfeef71ccedace3dbe56e197afdc8e084092
SniT's Macbook Pro <nguyentin082@gmail.com>
2025-07-21 00:55:39 +0700

    fix: config env url

--- client/core/config.ts
+++ client/core/config.ts
@@ -1,16 +1,10 @@
 // Load environment variables
 const config = {
     // Face Recognition API URL
-    FACE_AGENT_API_URL:
-        process.env.FACE_AGENT_API_URL ||
-        process.env.NEXT_PUBLIC_FACE_AGENT_API_URL ||
-        'http://localhost:8080',
+    FACE_AGENT_API_URL: process.env.NEXT_PUBLIC_FACE_RECOG_URL,
 
     // NestJS Server API URL
-    NEST_SERVER_API_URL:
-        process.env.NEST_SERVER_API_URL ||
-        process.env.NEXT_PUBLIC_NEST_SERVER_API_URL ||
-        'http://localhost:3000',
+    NEST_SERVER_API_URL: process.env.NEXT_PUBLIC_API_URL,
 };
 
 export default config;
